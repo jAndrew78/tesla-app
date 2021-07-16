cd56dcb281964d69f5c9910b9809743d550319ed
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-07-16 17:13:36 -0500

    replaced generic expo icons with Tesla logo and  changed background color in app.json, changed welcome text on App.js to test emulator was working properly

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Tesla</Text>
+      <Text>Hello  Tesla</Text>
       <StatusBar style="auto" />
     </View>
   );
