bc69bc5fa1c5550863f8d9490025c29494988ca7
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-07-17 02:14:37 -0500

    created cars file with list of cars

---
+++ src/fixtures/cars.js
@@ -0,0 +1,25 @@
+export default [
+  {
+    name: 'Model S',
+    tagline: 'Starting at $69,420',
+    image: require('../../assets/images/ModelS.png'),
+  }, 
+  {
+    name: 'Model 3',
+    tagline: 'Order Online for',
+    taglineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/Model3.png'),
+  }, 
+  {
+    name: 'Model X',
+    tagline: 'Order Online for',
+    taglineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/ModelX.png'),
+  }, 
+  {
+    name: 'Model Y',
+    tagline: 'Order Online for',
+    taglineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/ModelY.png'),
+  }
+];
