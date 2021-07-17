5814e57d39122b6fb025c208e4b17d554d5d3bb4
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-07-16 22:05:11 -0500

    changed to 2 spaces per tab - as it seems pretty standard

--- src/components/CarItem/index.jsx
+++ src/components/CarItem/index.jsx
@@ -3,22 +3,22 @@ import { Text, View, ImageBackground } from 'react-native';
 import styles from './styles/CarItem';
 
 const CarItem = props => {
-    return (
-        <View style={styles.carContainer}>
+  return (
+    <View style={styles.carContainer}>
 
-            <ImageBackground 
-                source={require('../../../assets/images/ModelS.png')}
-                style={styles.image}
-            />
+      <ImageBackground 
+        source={require('../../../assets/images/ModelS.png')}
+        style={styles.image}
+      />
 
-            <View style={styles.titles}>
-                <Text style={styles.title}>Model S</Text>
-                <Text style={styles.subtitle}>Starting at $69,420</Text>
-            </View>
+      <View style={styles.titles}>
+        <Text style={styles.title}>Model S</Text>
+        <Text style={styles.subtitle}>Starting at $69,420</Text>
+      </View>
 
-        </View>
-    );
-  }
+    </View>
+  );
+}
 
-  export default CarItem;
+export default CarItem;
   
