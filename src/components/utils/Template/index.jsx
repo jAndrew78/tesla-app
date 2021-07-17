8b032af9b27069da343dc22c535e49b431f69236
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-07-16 22:15:09 -0500

    created a template for component files/folders in components/utils

---
+++ src/components/utils/Template/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+import styles from './styles/Template';
+
+const Template = props => {
+  return (
+    <View style={styles.template}>
+      <Text>Template Component</Text>
+    </View>
+  );
+}
+
+export default Template;
